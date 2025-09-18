978213b2fce05140b55a50505995a1c4b86a35b7
Rain-62 <bkahraman619@gmail.com>
2025-09-18 21:03:40 +0200

    taschenrechner mit 2 variablen

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
+
 def add(x,y):
+    
     return x+y
 
 def sub(x,y):
+    
     return x-y
 
-print(add(2,4))
+def multiply(x,y):
+    
+    return x*y
 
-print(sub(100,39))
-
-print("hallo wieder ein test!! hehe")
-
-print("hello world")
-print("Dies ist ein Test!")
+def divide(x,y):
+    if y == 0:
+     return "Divison durch 0 geht nicht!"
+    return x/y
 
 
-print("mhmmmm")
+print("Please enter your 2 numbers")
+x = float(input("x = "))
+y = float(input("y = "))
 
-print("Hallo was geht?")
-print("test über github.com")
+print("Enter what you wanna do")
+
+funktion = input()
+if funktion == "add":
+    print(add(x,y))
+    
+elif funktion == "sub":
+    print(sub(x,y))
+
+elif funktion == "multiply":
+    print(multiply(x,y))
+
+elif funktion == "divide":
+    print(divide(x,y))
+
+else:
+ print("Didnt understand, try again!")
